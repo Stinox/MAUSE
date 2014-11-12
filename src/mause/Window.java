5d4fb499342e76135691037e4adcb926e300cff4
Joris1225 <jorisvanvugt@live.nl>
2014-11-12 22:52:45 +0100

    Changed Look and Feel

--- src/mause/Window.java
+++ src/mause/Window.java
@@ -4,6 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Window extends JFrame {
 	/**
@@ -13,7 +16,16 @@ public class Window extends JFrame {
 
 	public Window() {
 		// ’super ’ calls a function inherited from the parent class ( JFrame )
-		super();
+		for(LookAndFeelInfo info: UIManager.getInstalledLookAndFeels()){
+			System.out.println(info.getName());
+			if("Nimbus".equals(info.getName())){
+				try {
+					UIManager.setLookAndFeel(info.getClassName());
+				} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 		super.setTitle(" Mega Awesome Uber Super Epic - MAUSE ");
 		super.setSize(new Dimension(1600, 900));
 		// Make sure the window appears in the middle of your screen
@@ -22,15 +34,13 @@ public class Window extends JFrame {
 		super.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		// Chooses a certain layout type for the elements in this frame
 		super.getContentPane().setLayout(new BorderLayout());
-
+		
 		DrawingPanel rp = new DrawingPanel();
 		ButtonPanel bp = new ButtonPanel(rp);
 		// Places the RectPanel in the center of the frame
 		super.getContentPane().add(rp, BorderLayout.CENTER);
 		// Places the ButtonPanel in the top of the frame
 		super.getContentPane().add(bp, BorderLayout.NORTH);
-		
-
 		// Set the window to visible ! Yup ... This is necessary
 		super.setVisible(true);
 	}

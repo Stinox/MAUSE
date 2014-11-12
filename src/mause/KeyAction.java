15d81c90db8f778943e69d50b21ee2ff01741211
Joris1225 <jorisvanvugt@live.nl>
2014-11-12 22:52:28 +0100

    Added Timer for experiments

---
+++ src/mause/KeyAction.java
@@ -0,0 +1,38 @@
+package mause;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+public class KeyAction extends AbstractAction{
+
+	private static final long serialVersionUID = 1L;
+	long timer;
+	boolean running = false;
+	
+    public KeyAction(String actionCommand) {
+        putValue(ACTION_COMMAND_KEY, actionCommand);
+     }
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getActionCommand() == "timer"){
+			timer();
+		}
+		
+	}
+	
+	private void timer() {
+		if(!running){
+			timer = System.currentTimeMillis();
+			running = true;
+			System.out.println("Started timer");
+		}
+		else if(running){
+			running = false;
+			System.out.println("Stopped timer: " + Math.round((System.currentTimeMillis() - timer)/1000) + " seconds");
+		}
+	}
+
+
+}

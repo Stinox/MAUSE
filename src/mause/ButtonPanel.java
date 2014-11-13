9b02017f32cd526f8f454aa175964ef3384b1633
Joris1225 <jorisvanvugt@live.nl>
2014-11-13 23:29:08 +0100

    Added Tooltips

--- src/mause/ButtonPanel.java
+++ src/mause/ButtonPanel.java
@@ -27,53 +27,63 @@ public class ButtonPanel extends JPanel {
 		rect.addActionListener(new ToolButtonHandler(drawingPanel));
 		drawingPanel.addButton(rect);
 		rect.setBackground(selectedColor);
+		rect.setToolTipText("Click on the canvas and drag your mouse to draw a rectangle.");
 		
 		ToolButton ellipse = new ToolButton(new ImageIcon(MauseIcon.ellipse.getIcon()), Tool.ELLIPSE);
 		this.add(ellipse);
 		ellipse.addActionListener(new ToolButtonHandler(drawingPanel));
 		drawingPanel.addButton(ellipse);
+		ellipse.setToolTipText("Click on the canvas and drag your mouse to draw a circle.");
 		
 		ToolButton line = new ToolButton(new ImageIcon(MauseIcon.line.getIcon()), Tool.LINE);
 		this.add(line);
 		line.addActionListener(new ToolButtonHandler(drawingPanel));
 		drawingPanel.addButton(line);
+		line.setToolTipText("Click on the canvas and drag your mouse to draw a line.");
 		
 		ToolButton image = new ToolButton(new ImageIcon(MauseIcon.image.getIcon()), Tool.IMAGE);
 		this.add(image);
 		image.addActionListener(new ToolButtonHandler(drawingPanel));
 		drawingPanel.addButton(image);
+		image.setToolTipText("Click on the canvas and select a loccaly stored image in the dialogbox (most formats are supported).");
 		
 		ToolButton text = new ToolButton(new ImageIcon(MauseIcon.text.getIcon()), Tool.TEXT);
 		this.add(text);
 		text.addActionListener(new ToolButtonHandler(drawingPanel));
 		drawingPanel.addButton(text);
+		text.setToolTipText("Click on the canvas and enter your text and font size in the dialog box.");
 		
 		ToolButton move = new ToolButton(new ImageIcon(MauseIcon.move.getIcon()), Tool.MOVE);
 		this.add(move);
 		move.addActionListener(new ToolButtonHandler(drawingPanel));
 		drawingPanel.addButton(move);
+		move.setToolTipText("Use this tool to select and move shapes on the canvas.");
 		
 		ToolButton resize = new ToolButton(new ImageIcon(MauseIcon.resize.getIcon()), Tool.RESIZE);
 		this.add(resize);
 		resize.addActionListener(new ToolButtonHandler(drawingPanel));
 		drawingPanel.addButton(resize);
+		resize.setToolTipText("Use this tool to select shapes and resize them.");
 		
 		ToolButton foreground = new ToolButton(new ImageIcon(MauseIcon.foreground.getIcon()), Tool.FOREGROUND);
 		this.add(foreground);
 		foreground.addActionListener(new ToolButtonHandler(drawingPanel));
+		foreground.setToolTipText("Click this button to move the selected shape to the foreground");
 		
 		ToolButton background = new ToolButton(new ImageIcon(MauseIcon.background.getIcon()), Tool.BACKGROUND);
 		this.add(background);
 		background.addActionListener(new ToolButtonHandler(drawingPanel));
+		background.setToolTipText("Click this button to move the selected shape to the background");
 		
 		ToolButton delete = new ToolButton(new ImageIcon(MauseIcon.delete.getIcon()), Tool.DELETE);
 		this.add(delete);
 		delete.addActionListener(new ToolButtonHandler(drawingPanel));
 		drawingPanel.addButton(delete);
+		delete.setToolTipText("Select this tool and click on a shape to remove that shape.");
 		
 		JTextField sliderText = new JTextField("003");
 		JSlider slider = new JSlider();
-		slider.setToolTipText("Line Thickness");
+		slider.setToolTipText("Adjust the line thickness of rectangles, circles and lines");
 		slider.setValue(3);
 		this.add(slider);
 		this.add(sliderText);
@@ -99,5 +109,8 @@ public class ButtonPanel extends JPanel {
 		drawingPanel.setStroke(changeStrokeColor);
 		this.add(changeStrokeColor);
 		this.add(changeFillColor);
+		changeStrokeColor.setToolTipText("Select a color while this mode is selected to adjust the stroke color of rectangles, circles and lines");
+		changeFillColor.setToolTipText("Select a color while this mode is selected to adjust the fill color of rectangles, circles and lines");
+		
 	}
 }
